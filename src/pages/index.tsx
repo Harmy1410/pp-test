947566c3f1f9fa9dc4877d95bf50c17aad209f1e
Harmee Patel <harmeepatel@Harmees-MBP.hitronhub.home>
2022-06-27 20:30:20 -0400

    styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
     let name: string = 'hello world';
-    return <div className="max-w-md text-3xl font-bold">{name}</div>;
+    return <div className="max-w-md">{name}</div>;
 };
 
 export default Home;
